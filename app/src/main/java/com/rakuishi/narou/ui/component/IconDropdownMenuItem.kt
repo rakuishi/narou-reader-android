31edc2ec4e9834aebceb92c9cf5c1351fc564307
rakuishi <rakuishi@gmail.com>
2022-08-10 21:15:48 +0900

    Fix IconDropdownMenuItem

--- app/src/main/java/com/rakuishi/narou/ui/component/IconDropdownMenuItem.kt
+++ app/src/main/java/com/rakuishi/narou/ui/component/IconDropdownMenuItem.kt
@@ -2,17 +2,11 @@ package com.rakuishi.narou.ui.component
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.rakuishi.narou.R
 import com.rakuishi.narou.ui.theme.NarouReaderTheme
 
@@ -24,24 +18,16 @@ fun IconDropdownMenuItem(
 ) {
     DropdownMenuItem(
         text = {
-            Row(
-                modifier = Modifier
-                    .wrapContentSize(unbounded = true)
-                    .padding(horizontal = 16.dp)
+            Text(
+                style = MaterialTheme.typography.bodyLarge,
+                text = stringResource(textResId),
             )
-            {
-                Icon(
-                    painter = painterResource(iconResId),
-                    contentDescription = stringResource(textResId),
-                )
-                Text(
-                    style = MaterialTheme.typography.bodyLarge,
-                    text = stringResource(textResId),
-                    modifier = Modifier.padding(start = 16.dp),
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis,
-                )
-            }
+        },
+        leadingIcon = {
+            Icon(
+                painter = painterResource(iconResId),
+                contentDescription = stringResource(textResId),
+            )
         },
         onClick = onClick,
     )
