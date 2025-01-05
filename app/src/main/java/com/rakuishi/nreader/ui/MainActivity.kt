76bc1af084dd948c9f6c6258c6481965f2099c4d
rakuishi <rakuishi@gmail.com>
2025-01-05 15:27:29 +0900

    Support Edge-to-edge

--- app/src/main/java/com/rakuishi/nreader/ui/MainActivity.kt
+++ app/src/main/java/com/rakuishi/nreader/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.rakuishi.nreader.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -17,13 +18,14 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
 
         setContent {
             NReaderTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.background,
                 ) {
                     val navController = rememberNavController()
                     MainNavHost(app = application as App, navController = navController)
--- app/src/main/java/com/rakuishi/nreader/ui/theme/Theme.kt
+++ app/src/main/java/com/rakuishi/nreader/ui/theme/Theme.kt
@@ -1,24 +1,15 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
-
 package com.rakuishi.nreader.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.LocalUseFallbackRippleImplementation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -61,23 +52,9 @@ fun NReaderTheme(
         else -> LightColorScheme
     }
 
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            (view.context as Activity).window.statusBarColor = colorScheme.background.toArgb()
-            WindowCompat
-                .getInsetsController(window, view)
-                .isAppearanceLightStatusBars = !darkTheme
-        }
-    }
-
-    // @see https://cs.android.com/androidx/platform/frameworks/support/+/androidx-main:compose/integration-tests/material-catalog/src/main/java/androidx/compose/material/catalog/ui/theme/Theme.kt
-    // TODO: M3 MaterialTheme doesn't provide LocalIndication, remove when it does
-    CompositionLocalProvider(LocalUseFallbackRippleImplementation provides true) {
-        MaterialTheme(
-            colorScheme = colorScheme,
-            typography = Typography,
-            content = content
-        )
-    }
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
 }
