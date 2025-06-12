24474f29fbff3c79dfc8ed61136af9b032379e21
rakuishi <rakuishi@gmail.com>
2025-06-12 19:06:08 +0900

    Android 16

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.rakuishi.nreader"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.rakuishi.nreader"
         minSdk = 30
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
